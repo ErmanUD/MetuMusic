65a1eab2059a110491f67848eb5a280747cee57b
Erman Ufuk Demirci <ermanufukdemirci@gmail.com>
2022-12-30 12:11:01 +0300

    Add ScrollView to LibraryViewController

--- MetuMusic/Controllers/Core/LibraryViewController.swift
+++ MetuMusic/Controllers/Core/LibraryViewController.swift
@@ -1,10 +1,64 @@
 import UIKit
 
 class LibraryViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let playlistsVC = LibraryPlaylistsViewController()
+    private let albumsVC = LibraryAlbumsViewController()
+    
+    private let scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.isPagingEnabled = true
+        
+        return sv
+    }()
+    
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        
+        scrollView.delegate = self
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.contentSize = CGSize(width: view.width * 2, height: scrollView.height)
+        
+        addChildren()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = CGRect(
+            x: 0,
+            y: view.safeAreaInsets.top + 55,
+            width: view.width,
+            height: view.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom - 55
+        )
+    }
+    
+    // MARK: - ViewMethods
+    
+    private func addChildren() {
+        addChild(playlistsVC)
+        scrollView.addSubview(playlistsVC.view)
+        playlistsVC.view.frame = CGRect(x: 0, y: 0, width: view.width, height: scrollView.height)
+        playlistsVC.didMove(toParent: self)
+        
+        addChild(albumsVC)
+        scrollView.addSubview(albumsVC.view)
+        albumsVC.view.frame = CGRect(x: view.width, y: 0, width: view.width, height: scrollView.height)
+        albumsVC.didMove(toParent: self)
+
+    }
+}
+
+// MARK: - ScrollViewDelegate
+
+extension LibraryViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("Scrolled")
     }
 }
